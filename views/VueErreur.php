24a0595c9985fcc4b2e0b1b15046cc836b90cdb8
HasnaeChihi <65312423+HasnaeChihi@users.noreply.github.com>
2020-05-20 02:32:09 +0200

    Delete VueErreur.php

--- views/VueErreur.php
+++
@@ -1,6 +0,0 @@
-<?php  $titre = 'Gestion des élèves' ; ?>
-<?php ob_start(); ?>
-<p> Une erreur est survenue : <?= $msgErreur ?></p>
-<?php $contenu = ob_get_clean(); ?>
-<?php requice_once 'gabarit.php'; ?>
-       
