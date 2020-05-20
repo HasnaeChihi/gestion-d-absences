fe57eb1bd84d5d0412f2743c5f6d759d20cae150
HasnaeChihi <65312423+HasnaeChihi@users.noreply.github.com>
2020-05-20 00:23:42 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,27 +1,58 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>Gestion des élèves</title>
+    </head>
+<body>
+    <?php
+       $bdd = new PDO('mysql:host=localhost;dbname=ensat;charset=utf8', 'root', '');
+       $eleves = $bdd->query('select CNE,Nom,Prenom,Adresse,Ville,email, Photo, etat from eleves');
+    ?>        	
+<div id="global">
+<h1>Liste des élèves</h1>
+<table>
+	<tr>
+		<td>CNE</td>
+		<td>Nom</td>
+		<td>Prénom</td>
+		<td>Adresse</td>
+        <td>Ville</td>
+        <td>Email</td>
+        <td>Photo</td>
+        <td>etat</td>
+	</tr>
 <?php
-require('controller/controleur.php');
-try {
-if (isset($_GET['action'])) {
-if ($_GET['action'] == 'eleve') {
-if (isset($_GET['id'])) {
-$idEleve = intval($_GET['id']);
-if ($idEleve != 0)
-eleve($idEleve);
-else
-throw new Exception("Identifiant d élève non valide");
+foreach ($eleves as $eleve) {
+	$et="";
+	$lien="";
+	if($eleve["etat"]=="true")
+	{
+		$et="active";
+		$lien="activer.php?cne=".$eleve["CNE"]."&etat=false";
+	}
+	else
+	{
+		$et="desactive";
+		$lien="activer.php?cne=".$eleve["CNE"]."&etat=true";
+	}
+	?>
+		<tr>
+		<td><?php echo $eleve["CNE"]; ?></td>
+		<td><?php echo $eleve["Nom"]; ?></td>
+		<td><?php echo $eleve["Prenom"]; ?></td>
+        <td><?php echo $eleve["Adresse"]; ?></td>
+        <td><?php echo $eleve["Ville"]; ?></td>
+        <td><?php echo $eleve["email"]; ?></td>
+        <td><?php echo $eleve["Photo"]; ?></td>
+		<td><a href="<?php echo $lien; ?>"><?php echo $et; ?></a></td>
+	</tr>
+	<?php
+	
 }
-else
-throw new Exception("Identifiant d élève non défini");
-}
-else
-throw new Exception("Action non valide");
-}
-else {
-accueil(); // action par défaut
-}
-}
-catch (Exception $e) {
-erreur($e->getMessage());
-}
-?>
-
+	?>
+</table>	
+</div>
+</body>
+</html>
